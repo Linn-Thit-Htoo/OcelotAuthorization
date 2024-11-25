e1a28e09f2ab95d72786fdfaed00064d930ab3de
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-25 13:36:48 +0630

    -

--- OcelotAuthorization.Auth/Program.cs
+++ OcelotAuthorization.Auth/Program.cs
@@ -7,10 +7,7 @@ builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,7 +16,6 @@ builder.Services.AddScoped<JWTAuth>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
