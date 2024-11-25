a40ff28252b1c3bcf8da87647a46040a3ac9eec1
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-25 13:36:28 +0630

    -

--- OcelotAuthorization.Auth/Extensions/DependencyInjectionExtension.cs
+++ OcelotAuthorization.Auth/Extensions/DependencyInjectionExtension.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace OcelotAuthorization.Auth.Extensions;
 
 public static class DependencyInjectionExtension
 {
     public static IServiceCollection AddAuthenticationService(
-this IServiceCollection services,
-WebApplicationBuilder builder
-)
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
         builder
             .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -26,7 +26,7 @@ WebApplicationBuilder builder
                     ValidIssuer = builder.Configuration["Jwt:Issuer"],
                     IssuerSigningKey = new SymmetricSecurityKey(
                         Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-                    )
+                    ),
                 };
             });
 
