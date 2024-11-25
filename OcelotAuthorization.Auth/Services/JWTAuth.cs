a610612fcb31c593fe7eaac6804f9ee4571ff339
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-25 13:36:34 +0630

    -

--- OcelotAuthorization.Auth/Services/JWTAuth.cs
+++ OcelotAuthorization.Auth/Services/JWTAuth.cs
@@ -3,52 +3,51 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace OcelotAuthorization.Auth.Services
+namespace OcelotAuthorization.Auth.Services;
+
+public class JWTAuth
 {
-    public class JWTAuth
+    private readonly IConfiguration _configuration;
+
+    public JWTAuth(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public JWTAuth(IConfiguration configuration)
+    public string GetJWTToken(string userId, string email, string role, string[] scopes)
+    {
+        try
         {
-            _configuration = configuration;
+            var claims = new List<Claim>
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, userId),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+            new Claim("UserId", userId),
+            new Claim("Email", email),
+            new Claim(ClaimTypes.Role, role)
+        };
+
+            foreach (var scope in scopes)
+            {
+                claims.Add(new Claim("scope", scope));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(10),
+                signingCredentials: signIn
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
-
-        public string GetJWTToken(string userId, string email, string role, string[] scopes)
+        catch (Exception ex)
         {
-            try
-            {
-                var claims = new List<Claim>
-                {
-                new Claim(JwtRegisteredClaimNames.Sub, userId),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
-                new Claim("UserId", userId),
-                new Claim("Email", email),
-                new Claim(ClaimTypes.Role, role)
-            };
-
-                foreach (var scope in scopes)
-                {
-                    claims.Add(new Claim("scope", scope));
-                }
-
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
-                var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                var token = new JwtSecurityToken(
-                    _configuration["Jwt:Issuer"],
-                    _configuration["Jwt:Audience"],
-                    claims,
-                    expires: DateTime.UtcNow.AddHours(10),
-                    signingCredentials: signIn
-                );
-
-                return new JwtSecurityTokenHandler().WriteToken(token);
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            throw ex;
         }
     }
 }
