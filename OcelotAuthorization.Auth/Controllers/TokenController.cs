f89c1e6f3e21cfdde067f9a3adf286a77176f323
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-25 13:36:09 +0630

    -

--- OcelotAuthorization.Auth/Controllers/TokenController.cs
+++ OcelotAuthorization.Auth/Controllers/TokenController.cs
@@ -1,26 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OcelotAuthorization.Auth.Services;
 
-namespace OcelotAuthorization.Auth.Controllers
+namespace OcelotAuthorization.Auth.Controllers;
+
+[Route("api")]
+[ApiController]
+public class TokenController : ControllerBase
 {
-    [Route("api")]
-    [ApiController]
-    public class TokenController : ControllerBase
+    internal readonly JWTAuth _jWTAuth;
+
+    public TokenController(JWTAuth jWTAuth)
     {
-        internal readonly JWTAuth _jWTAuth;
+        _jWTAuth = jWTAuth;
+    }
 
-        public TokenController(JWTAuth jWTAuth)
-        {
-            _jWTAuth = jWTAuth;
-        }
+    [HttpPost("GenerateToken")]
+    public IActionResult GenerateToken()
+    {
+        string[] scopes = { "blog.read" };
+        string token = _jWTAuth.GetJWTToken(Guid.NewGuid().ToString(), "mglinnthithtoo@gmail.com", "admin", scopes);
 
-        [HttpPost("GenerateToken")]
-        public IActionResult GenerateToken()
-        {
-            string[] scopes = { "blog.read" };
-            string token = _jWTAuth.GetJWTToken(Guid.NewGuid().ToString(), "mglinnthithtoo@gmail.com", "admin", scopes);
-
-            return Ok(token);
-        }
+        return Ok(token);
     }
 }
